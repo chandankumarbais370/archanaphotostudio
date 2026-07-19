64d8f387bcb47a1e7861e9aa040a6426a722e12d
archanaphotostudio <chandankumarbais370@gmail.com>
2026-07-19 12:56:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -372,3 +372,22 @@ backTop.addEventListener("click",(e)=>{
     });
 
 });
+//=========================
+// SCROLL PROGRESS BAR
+//=========================
+
+const progressBar = document.getElementById("progressBar");
+
+window.addEventListener("scroll",()=>{
+
+    const scrollTop = document.documentElement.scrollTop;
+
+    const scrollHeight =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+
+    const progress = (scrollTop / scrollHeight) * 100;
+
+    progressBar.style.width = progress + "%";
+
+});
